468f684570da15318c4bdcc6d9b5849897bcfb6c
chenyuany <1584216202@qq.com>
2017-05-27 10:34:19 +0800

    ceshi

---
+++ testCase/login/test_person.py
@@ -0,0 +1,33 @@
+#-*- coding:utf-8 -*-
+''' 
+#文件名：
+#作者：陈圆圆
+#创建日期：
+#模块描述：
+#历史修改记录
+#修改人：
+#修改日期：
+#修改内容：
+'''
+from selenium import webdriver
+class InitDriver:
+	print("")
+	for list_date in list:
+		pass
+	driver=webdriver.Chrome()
+	ip_address="192.16.10.249"
+	def  add_person_T068(self):
+		pass
+	def  mod_person_T069(self):
+		pass
+	def  del_person_T070(self):
+		pass
+	def  find_person (self):
+		pass
+	def add_argument(self,argument):
+		pass
+class Preson(object):
+	print("hifdhaifhfisaf")
+	#用户名
+LOGIN_USER = "cyy"
+LOGIN_BUTTON_DENGLU = "button"
