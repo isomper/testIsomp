778df6f5a1f82b220deada90b0dded42264bbcdb
guayru <979553618@qq.com>
2017-06-27 15:50:12 +0800

    update driver

--- testSuite/test_1login_suite.py
+++ testSuite/test_1login_suite.py
@@ -6,16 +6,22 @@ sys.setdefaultencoding('utf-8')
 #导入驱动
 sys.path.append("/testIsomp/common/")
 from _initDriver import *
+from _globalVal import globalValue
 
 #导入登录
 sys.path.append("/testIsomp/testCase/login")
 from test_login import *
 
+
+
 import unittest
 
 class testLoginSuite(unittest.TestCase):
     def setUp(self):
-        self.browser = initDriver().open_driver()
+        driver_lists = globalValue().get_value()
+        self.browser = initDriver().remote_open_driver(driver_lists[0],driver_lists[1])
+        
+#        self.browser = initDriver().open_driver()
         
     def test_login(self):
         test_login = testLogin(self.browser)
