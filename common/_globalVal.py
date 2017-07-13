a24a06d1f148e001aeedc49226799a1c9679bf93
guayru <979553618@qq.com>
2017-07-13 18:18:30 +0800

    add comment

--- common/_globalVal.py
+++ common/_globalVal.py
@@ -8,25 +8,17 @@ sys.path.append("/testIsomp/common/")
 from _transCoding import jsonTranscoding
 from _initDriver import initDriver
 
+u'''设置全局变量'''
 class globalValue():
+	
+	#设置全局变量
 	def set_value(self,hostUrl,browerType):
 		global host
 		global brower
-		global status
+
 		host = hostUrl
 		brower = browerType
 		
+	#获取全局变量	
 	def get_value(self):
 		return host,brower
-	
-#global_value().set_value("http://192.166.23.3:5555/wb/hub","chrome")
-#print global_value().get_value()
-#def set_value(host_url,brower_type):
-#	global host
-#	global brower
-#	host = host_url
-#	brower = brower_type
-#	
-#def get_value():
-#	return host,brower
-#
--- common/_transCoding.py
+++ common/_transCoding.py
@@ -6,26 +6,26 @@ import json
 u'''解码json文件'''
 class jsonTranscoding():
     
-    u'''读取文件'''
+    u'''文件格式转换为json'''
     def loadFont(self):
-        f = open("\\testIsomp\\common\\config.json","r")
-        setting = json.load(f)
-        return setting
+        file = open("\\testIsomp\\common\\config.json","r")
+        json_setting = json.load(file)
+        return json_setting
     
     u'''获取ip'''
     def get_app_ip(self):
-        t = self.loadFont()
-        app_dict = t["APP"]
+        json_object = self.loadFont()
+        app_dict = json_object["APP"]
         app_ip = app_dict['appip']
         return app_ip
     
     u'''获取远程服务器url和浏览器类型'''
     def set_brower(self):
-        t = self.loadFont()
-        key_num = len(t["BROWER"])
+        json_object = self.loadFont()
+        key_num = len(json_object["BROWER"])
         remote_dict = {}
         for key_index in range(key_num):
-            brower_key = t["BROWER"][key_index]
+            brower_key = json_object["BROWER"][key_index]
             ip = brower_key["ip"]
             port = brower_key["port"]
             type = brower_key["type"]
@@ -36,28 +36,28 @@ class jsonTranscoding():
 
         return remote_dict
 
-    u'''获取ip'''
+    u'''测试获取ip'''
     def get_app_ip_test(self):
-        t = self.loadFont()
-        ip_num = len(t["APP"])
+        json_object = self.loadFont()
+        ip_num = len(json_object["APP"])
         appip_list = []
         for ip_index in range(ip_num):
-            appip = t["APP"][ip_index]["appip"]
+            appip = json_object["APP"][ip_index]["appip"]
             appip_list.append(appip)  
         return appip_list
     
     u'''获取详细的浏览器类型'''
     def get_brower_type(self):
-        t = self.loadFont()
-        brower_num = len(t["BROWER"])
+        json_object = self.loadFont()
+        brower_num = len(json_object["BROWER"])
         brower_type_list = []
         for brower_index in range(brower_num):
-            brower_detail_type = t["BROWER"][brower_index]["detailType"]
+            brower_detail_type = json_object["BROWER"][brower_index]["detailType"]
             brower_type_list.append(brower_detail_type)  
         return brower_type_list
         
 
 
-#print jsonTranscoding().get_app_ip_test()
+#print jsonTranscoding().get_app_ip()
 #print jsonTranscoding().set_brower()
 #print jsonTranscoding().get_brower_type()
