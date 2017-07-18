5a701d4980fcac09ecc9d65d2f17f72f5b07d6c4
guayru <979553618@qq.com>
2017-07-18 11:23:49 +0800

    提交认证方式/登陆/用户case文件

--- testCase/login/test_login.py
+++ testCase/login/test_login.py
@@ -4,11 +4,6 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 from threading import Thread
-from selenium import webdriver
-from selenium.webdriver import Remote
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
 
 sys.path.append("/testIsomp/common/")
 from _excelRead import excelRead
@@ -33,70 +28,83 @@ class testLogin(object):
     def __init__(self,driver):
         self.driver = driver
         self.log = log()
+        self.loginFun = loginPage(self.driver)
+        self.cmf = commonFun(self.driver)
+        self.dataFile = dataFileName()
+
+    u'''获取测试数据
+    	Parameters:
+    		- sheetname:sheet名称
+    		return：表格数据
+    '''
+    def get_table_data(self,sheetname):
+    	filePath = self.dataFile.get_login_test_data_url()
+    	loginData = self.dataFile.get_data(filePath,sheetname)
+    	return loginData
+    	
+    u'''登陆的div弹窗的xpath'''
+    def login_msg(self):
+    	login_msg = "html/body/div[1]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr[2]/td[2]/div"
+    	return login_msg
+    
+    u'''根据访问方式类型登录测试
+            parameters:
+                sheetname : 表单名称
+    '''
+    def login_type(self,sheetname):
+        loginMes = self.login_msg()
+        loginData = self.get_table_data(sheetname)
+        #无检查点的测试项标识，如果为True说明通过
+        flag = False
+        for dataRow in range(len(loginData)):
+            #把单行的数据赋值给列表data
+            data = loginData[dataRow]
+            
+            try:
+                #如果不是第一行标题，则读取数据
+                if dataRow != 0:
+                    if sheetname == 'default':
+                        self.loginFun.login(data)
+#                           if dataRow == 1:
+#                               loginFun.set_max_login_count()
+                    elif sheetname == 'ad':
+                        self.loginFun.ad_login(data)
+                    elif sheetname == 'pwd_ad':
+                        self.loginFun.ad_pwd_login(data)
+                    elif sheetname == 'radius':
+                        self.loginFun.radius_pwd_login(data)
+                        
+                        #如果登陆成功，点击退出
+                    if self.loginFun.is_login_success():
+                        self.loginFun.quit()
+                        #设定没有检查点的测试项通过
+                        flag = True
+                        
+                    self.cmf.test_win_check_point("xpath",loginMes,data,flag)
+                    
+                    #清空标识状态
+                    flag = False
+                    
+            except Exception as e: 
+                self.log.print_detail("login type error",e)
+
+        
     
     #登陆测试
     def login(self):
         self.log.log_start("login")
-        
         u'''可以循环设定数据测试系统登录'''
-        dataFile = dataFileName()
-        loginPath = dataFile.get_login_test_data_url()
         sheets_name = ['default','ad','pwd_ad','radius']#'default','ad','pwd_ad','radius'
         for sheetname in sheets_name:
-            loginData = dataFile.get_data(loginPath,sheetname)
-        
-            #实例化login
-            loginFun = loginPage(self.driver)
-        
-            #实例化commonFun
-            cmf = commonFun(self.driver)
-        
-            #登陆的div弹窗的xpath
-            loginMes = "html/body/div[1]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr[2]/td[2]/div"
-        
-            #无检查点的测试项标识，如果为True说明通过
-            flag = False
-            for dataRow in range(len(loginData)):
-                #把单行的数据赋值给列表data
-                data = loginData[dataRow]
-            
-                try:
-                    #如果不是第一行标题，则读取数据
-                    if dataRow != 0:
-                        if sheetname == 'default':
-                            loginFun.login(data)
-#                            if dataRow == 1:
-#                                loginFun.set_max_login_count()
-                        elif sheetname == 'ad':
-                            loginFun.ad_login(data)
-                        elif sheetname == 'pwd_ad':
-                            loginFun.ad_pwd_login(data)
-                        elif sheetname == 'radius':
-                            loginFun.radius_pwd_login(data)
-                        
-                            #如果登陆成功，点击退出
-                        if loginFun.is_login_success():
-                            loginFun.quit()
-                            #设定没有检查点的测试项通过
-                            flag = True
-                        
-                        cmf.test_win_check_point("xpath",loginMes,data,flag)
-                    
-                        #清空标识状态
-                        flag = False
-                    
-                except Exception as e: 
-                    self.log.print_detail("login type error",e)
-#                    print "login type error:" + str(e)
-        
+            self.login_type(sheetname)
         self.log.log_end("login")
         
         
-if __name__ == "__main__":
-    
-    driver = initDriver().open_driver()
-    testLogin(driver).login()
-    
+#if __name__ == "__main__":#internet explorer
+#    browser = setDriver().set_driver()
+#    testLogin = testLogin(browser)
+#    testLogin.login()
+
     
     '''
     lists = jsonTranscoding().set_brower()
